b2064100e4ce335c77a9c16f860e656d7f052e0a
Nathan Edwards <nathanedwardsuk@gmail.com>
2025-04-09 17:05:18 +0200

    small formatting change

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -30,11 +30,13 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = [".herokuapp.com",
-                 "localhost",
-                 "127.0.0.1",
-                 "nathanedwards.dev",
-                 "www.nathanedwards.dev"]
+ALLOWED_HOSTS = [
+    ".herokuapp.com",
+    "localhost",
+    "127.0.0.1",
+    "nathanedwards.dev",
+    "www.nathanedwards.dev",
+]
 
 CSRF_TRUSTED_ORIGINS = ["https://*.herokuapp.com"]
 
