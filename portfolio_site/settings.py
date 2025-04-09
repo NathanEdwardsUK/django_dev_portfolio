c6c328d01c43a2a0694f4dcc4135b53ca397240e
Nathan Edwards <nathanedwardsuk@gmail.com>
2025-04-09 16:16:33 +0200

    Added www.nathanedwards.dev to ALLOWED_HOSTS

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -30,8 +30,11 @@ SECRET_KEY = env.str("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env.bool("DEBUG", default=False)
 
-ALLOWED_HOSTS = [".herokuapp.com", "localhost",
-                 "127.0.0.1", "nathanedwards.dev"]
+ALLOWED_HOSTS = [".herokuapp.com",
+                 "localhost",
+                 "127.0.0.1",
+                 "nathanedwards.dev",
+                 "www.nathanedwards.dev"]
 
 CSRF_TRUSTED_ORIGINS = ["https://*.herokuapp.com"]
 
