b8eb5e3bdaa5ac33905f7a30824713e1ac111496
Nathan Edwards <nathanedwardsuk@gmail.com>
2025-04-09 18:04:48 +0200

    Added nearly empty pages for climbing, cv and personal statement, prime number photo, pineapple poker. Added static/img to pages app and added prime number photo of my grandad, and my cv.

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,16 @@
 from django.urls import path
-from .views import HomeView
+from .views import (
+    ClimbingView,
+    HomeView,
+    PineapplePokerView,
+    PrimePhotoView,
+    ProfessionalView,
+)
 
 urlpatterns = [
     path("", HomeView, name="home"),
+    path("professional/", ProfessionalView, name="professional"),
+    path("pineapple/", PineapplePokerView, name="pineapple_poker"),
+    path("prime/", PrimePhotoView, name="prime_photo"),
+    path("climbing/", ClimbingView, name="climbing"),
 ]
--- pages/views.py
+++ pages/views.py
@@ -5,3 +5,23 @@ from django.contrib.auth.decorators import login_required
 @login_required
 def HomeView(request):
     return render(request, "pages/home.html")
+
+
+@login_required
+def ProfessionalView(request):
+    return render(request, "pages/professional.html")
+
+
+@login_required
+def PrimePhotoView(request):
+    return render(request, "pages/prime_photo.html")
+
+
+@login_required
+def PineapplePokerView(request):
+    return render(request, "pages/pineapple.html")
+
+
+@login_required
+def ClimbingView(request):
+    return render(request, "pages/climbing.html")
