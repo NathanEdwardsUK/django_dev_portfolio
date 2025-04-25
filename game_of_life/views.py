482f71442faf159aed65792f7abff2d11b5fdf8f
Nathan Edwards <nathanedwardsuk@gmail.com>
2025-04-25 18:40:33 +0200

    Made game of life sandbox page not require a login

--- game_of_life/views.py
+++ game_of_life/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_not_required
 
 
+@login_not_required
 def sandbox_view(request):
     return render(request, "game_of_life/sandbox.html")
     
